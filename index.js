5c68bf0d90c3d5f4ac735491773ae9d9ab132770
eve-atieno <atienoevelyne53@gmail.com>
2022-12-12 13:53:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+    //n steps
+   const complement = target - array[i]
+   for (let j =i + 1; j < array.length; j++){
+    //n*n steps
+    if(array[j] === complement)return true
+   } 
+  }
+  return false;
+  
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime complexity will be o(n^2)
+  space complexity 
 */
 
 /* 
   Add your pseudocode here
+  hasTargetSum([22, 19, 4, 6, 30], 25);
+  returns true because sum of 19 and 6 is 25.
+  iterate through each number in the array and identify a complement that adds to the target.(complement= target -number)
+  if there is complement 
+  return true
+  if i reach end of array with no complement
+   return false.
 */
 
 /*
@@ -29,6 +47,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([8, 9, 5], 14));
 }
 
 module.exports = hasTargetSum;
